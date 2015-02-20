56702f5ae4c6d300a99ab07d0516dc20bcec67a7
Towhidn <Towhidn@gmail.com>
2015-02-20 11:43:47 +0330

    issue with loading style & JS files is solved

--- qa-tabs-layer.php
+++ qa-tabs-layer.php
@@ -1,16 +1,25 @@
 <?php
 
 	class qa_html_theme_layer extends qa_html_theme_base {
-
+		var $plugin_directory;
+		var $plugin_url;
+		function qa_html_theme_layer($template, $content, $rooturl, $request)
+		{
+			global $qa_layers;
+			$this->plugin_directory = $qa_layers['Tabs Layer']['directory'];
+			$this->plugin_url = $qa_layers['Tabs Layer']['urltoroot'];
+			qa_html_theme_base::qa_html_theme_base($template, $content, $rooturl, $request);
+		}
+		
 		function head_css() {
 			qa_html_theme_base::head_css();
 			if (qa_opt('tw_custom_css')!='NO Costume Style')
-				$this->output('<link href="' . qa_opt('site_url'). QA_HTML_THEME_LAYER_URLTOROOT . 'styles/' . qa_opt('tw_custom_css') . '.css" type="text/css" rel="stylesheet"></link>');
+				$this->output('<link href="' . qa_opt('site_url') . $this->plugin_url . 'styles/' . qa_opt('tw_custom_css') . '.css" type="text/css" rel="stylesheet"></link>');
 		}
 		function head_script()
 		{
 			qa_html_theme_base::head_script();
-			$this->output('<script src="' . qa_opt('site_url'). QA_HTML_THEME_LAYER_URLTOROOT .'tabs.js"></script>');
+			$this->output('<script src="' . qa_opt('site_url') . $this->plugin_url . 'tabs.js"></script>');
 			
 		}
 	}
