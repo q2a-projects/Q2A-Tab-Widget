f8c9c4106407f81c92bd435096d2652438ddbdf5
Towhidn <Towhidn@gmail.com>
2015-02-20 14:35:13 +0330

    solved compatibility issue with webmaster plugin

--- qa-tabs-layer.php
+++ qa-tabs-layer.php
@@ -1,25 +1,23 @@
 <?php
 
 	class qa_html_theme_layer extends qa_html_theme_base {
-		var $plugin_directory;
-		var $plugin_url;
+		var $tabs_plugin_url;
 		function qa_html_theme_layer($template, $content, $rooturl, $request)
 		{
 			global $qa_layers;
-			$this->plugin_directory = $qa_layers['Tabs Layer']['directory'];
-			$this->plugin_url = $qa_layers['Tabs Layer']['urltoroot'];
+			$this->tabs_plugin_url = $qa_layers['Tabs Layer']['urltoroot'];
 			qa_html_theme_base::qa_html_theme_base($template, $content, $rooturl, $request);
 		}
 		
 		function head_css() {
 			qa_html_theme_base::head_css();
 			if (qa_opt('tw_custom_css')!='NO Costume Style')
-				$this->output('<link href="' . qa_opt('site_url') . $this->plugin_url . 'styles/' . qa_opt('tw_custom_css') . '.css" type="text/css" rel="stylesheet"></link>');
+				$this->output('<link href="' . qa_opt('site_url') . $this->tabs_plugin_url . 'styles/' . qa_opt('tw_custom_css') . '.css" type="text/css" rel="stylesheet"></link>');
 		}
 		function head_script()
 		{
 			qa_html_theme_base::head_script();
-			$this->output('<script src="' . qa_opt('site_url') . $this->plugin_url . 'tabs.js"></script>');
+			$this->output('<script src="' . qa_opt('site_url') . $this->tabs_plugin_url . 'tabs.js"></script>');
 			
 		}
 	}
